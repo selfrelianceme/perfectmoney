e187146f09c0aca05136ee66c38152e7a8adc330
Mark Klein <devfutures.com@gmail.com>
2020-01-16 13:01:22 +0200

    change cancel url

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::post('perfectmoney/cancel', 'Selfreliance\PerfectMoney\PerfectMoney@cancel_payment')->name('perfectmoney.cancel');
+Route::any('perfectmoney/cancel', 'Selfreliance\PerfectMoney\PerfectMoney@cancel_payment')->name('perfectmoney.cancel');
 Route::post('perfectmoney/confirm', 'Selfreliance\PerfectMoney\PerfectMoney@validateIPNRequest')->name('perfectmoney.confirm');
 Route::any('perfectmoney/personal', function(){
 	return redirect(Config::get('perfectmoney.to_account'));
